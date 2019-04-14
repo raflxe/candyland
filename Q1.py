d340053846c6513c9132bcff2eb0567a2eb74933
Rafael Chagas <raflxe@users.noreply.github.com>
2019-04-14 19:03:16 -0300

    Finale version without test
    Versao final sem teste

--- Q1.py
+++ Q1.py
@@ -1,9 +1,10 @@
 def validaConjunto(entrada):
-	validos= set(".","0","1","2","3","4","5","6","7","8","9")
+	validos = {".","0","1","2","3","4","5","6","7","8","9"}
 	if validos in entrada:
-		return true
+		auxiliar = true
 	else:
 		return false
+	return auxiliar
 
 def valida(entrada):
 	if entrada.isDigit:
@@ -12,9 +13,22 @@ def valida(entrada):
 		if entrada[1:len(entrada)].isDigit:
 			return true
 		else:
-			validaConjunto(entrada[1:len(entrada)])
+			return validaConjunto(entrada[1:len(entrada)])
 	else:
 		if validaConjunto(entrada):
 			return true
 		else:
 			return false
+			
+numerosIntReal = input()
+lista = [None]
+aux = 0
+if numerosIntReal != "" :
+    while (numerosIntReal != ""):
+        if valida(numerosIntReal):
+            lista.append() = numerosIntReal
+            aux += 1
+            numerosIntReal = input()           
+    print("Lista de Números Válidos Lidos = ", lista)
+else:
+    print("Lista de Números Válidos Lidos = []")
